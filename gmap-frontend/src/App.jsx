87059b5db99a1771fc60bf1b5869322b1f74dc9b
GathangaElijah <elyg3672@gmail.com>
2025-07-09 14:25:45 +0300

    feat: created a location tracker card

--- gmap-frontend/src/App.jsx
+++ gmap-frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Map from './Map';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
+      <div className='header-card'>
+        <h2>My Location Tracker</h2>
+        <p>This map shows your real-time location. Move around to see</p>
+      </div>
       <Map />
     </div>
   );
